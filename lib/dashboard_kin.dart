354cc98f9e2f503ab1e51c2237750e5507b3c6c2
anshiiiii <vsatyana@gitam.in>
2024-05-29 16:04:19 +0530

    Kin logout feature added

--- lib/dashboard_kin.dart
+++ lib/dashboard_kin.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:solikin/main.dart'; // Import the HomePage for redirection
 
 class DashboardKinPage extends StatelessWidget {
+  Future<void> _logout(BuildContext context) async {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomePage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Kin Dashboard'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _logout(context),
+          ),
+        ],
       ),
       body: Center(
         child: Text('Welcome, Kin!', style: TextStyle(fontSize: 24)),
